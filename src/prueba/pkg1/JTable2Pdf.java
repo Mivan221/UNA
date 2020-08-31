b6ba1b4a525a17d264f627c0f4c608f9d7c68906
Evis98 <evaduranescobar@gmail.com>
2020-08-30 20:41:33 -0600

    Cambios

---
+++ src/prueba/pkg1/JTable2Pdf.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba.pkg1;
+
+/**
+ *
+ * @author Ivan
+ */
+import java.awt.BorderLayout;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.io.FileOutputStream;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class JTable2Pdf extends JFrame {
+     private JTable table;
+
+     public JTable2Pdf() {
+       getContentPane().setLayout(new BorderLayout());
+       createTable();
+     }
+     private void createTable() {
+         Object[][] data = {
+                   {"Juan", "SOto",
+                    "SnowboardingXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", new Integer(5), new Boolean(false)},
+                   {"Miguel", "Montero",
+                    "Rowing", new Integer(3), new Boolean(true)},
+                   {"Steph", "Vargas",
+                    "Knitting", new Integer(2), new Boolean(false)},
+                   {"Enrique", "Escobar",
+                    "Speed reading", new Integer(20), new Boolean(true)},
+                   {"Jose", "Brown",
+                    "Pool", new Integer(10), new Boolean(false)}
+                   };
+         String[] columnNames = {"First Name",
+                 "Last Name",
+                 "Sport",
+                 "# of Years",
+                 "Vegetarian"};
+
+       table = new JTable(data, columnNames);
+
+       JPanel tPanel = new JPanel(new BorderLayout());
+       tPanel.add(table.getTableHeader(), BorderLayout.NORTH);
+       tPanel.add(table, BorderLayout.CENTER);
+
+       getContentPane().add(tPanel, BorderLayout.CENTER);
+     }
+     private void print() {
+       Document document = new Document(PageSize.A4.rotate());
+       try {
+         PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\PC\\OneDrive\\Documentos\\GitHub\\UNA\\jTable.pdf"));
+
+         document.open();
+         PdfContentByte cb = writer.getDirectContent();
+
+         cb.saveState();
+         Graphics2D g2 = cb.createGraphics(500, 500);
+
+         Shape oldClip = g2.getClip();
+         g2.clipRect(10, 0, 500, 500);
+
+         table.print(g2);
+         g2.setClip(oldClip);
+
+         g2.dispose();
+         cb.restoreState();
+       } catch (Exception e) {
+         System.err.println(e.getMessage());
+       }
+       document.close();
+     }
+     public static void main(String[] args) {
+       JTable2Pdf frame = new JTable2Pdf();
+       frame.pack();
+       frame.setVisible(true);
+       frame.print();
+     }
+}
